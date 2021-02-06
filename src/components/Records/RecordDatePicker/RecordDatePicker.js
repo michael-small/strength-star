fc6635b02291021732ec64bdf7c68751d59eb659
michael-small <small203@morris.umn.edu>
2021-02-06 13:24:18 -0600

    Add material-io date picker component
    Functional component. Needs to use a 1.x package (2+ currently) for
    material-ui, documented in a comment in the component.

--- src/components/Records/RecordDatePicker/RecordDatePicker.js
+++ src/components/Records/RecordDatePicker/RecordDatePicker.js
@@ -1,12 +1,41 @@
-import React, { Component } from 'react';
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
-export default class RecordDatePicker extends Component {
-	render() {
-		return (
-			<div>
-				<h2>RecordDatePicker</h2>
-				<i>Some sort of date picker, probably from `material-ui`</i>
-			</div>
-		);
-	}
+// "Important: For material-ui-pickers v3 use v1.x version of @date-io adapters."
+// https://material-ui-pickers.dev/getting-started/installation#peer-library
+// The standalone date-fns package is 2+ as of this commit, but it seems fine independently of date-io package
+import DateFnsUtils from '@date-io/date-fns';
+
+import {
+	MuiPickersUtilsProvider,
+	KeyboardTimePicker,
+	KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+export default function RecordDatePicker() {
+	// The first commit of Material-UI
+	const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+	const handleDateChange = (date) => {
+		setSelectedDate(date);
+	};
+
+	return (
+		<MuiPickersUtilsProvider utils={DateFnsUtils}>
+			<Grid container>
+				<KeyboardDatePicker
+					margin='normal'
+					id='date-picker-dialog'
+					label='Date picker dialog'
+					format='MM/dd/yyyy'
+					value={selectedDate}
+					onChange={handleDateChange}
+					KeyboardButtonProps={{
+						'aria-label': 'change date',
+					}}
+				/>
+			</Grid>
+		</MuiPickersUtilsProvider>
+	);
 }
