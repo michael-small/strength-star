a6605a2605102bd29dcfe625f8bbc2ee290796e8
michael-small <small203@morris.umn.edu>
2021-02-07 13:32:38 -0600

    Utilize DayRecord CSS file using CSS modules
    What wasn't clear from Max's course at a glance awhile later, is that
    this project must be missing some CSS import. The course project
    uses normall CSS files, but a guide I found (lniked below) said that
    it is a crucial distinction to name files with "file.module.css" to
    make a local scope. For now I am trying this.

--- src/components/Records/DayRecord/DayRecord.js
+++ src/components/Records/DayRecord/DayRecord.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import classes from './DayRecord.css';
+import classes from './DayRecord.module.scss';
 
 export default class DayRecord extends Component {
 	render() {
@@ -19,26 +19,20 @@ export default class DayRecord extends Component {
 			}
 		`;
 
-		const StyledDiv = styled.div`
-			border: 1px solid black;
-			margin-bottom: 0.5rem;
-			padding: 0.5rem;
-		`;
-
 		return (
-			<StyledDiv>
+			<div className={classes.Day}>
 				{/* Of course these will be props that `AllDayRecords` will iterate on later, I'm just lazy right now and about to go to work*/}
 				<h2>DayRecord</h2>
 				<b>Lbs: 25</b> &nbsp;&nbsp;&nbsp; <b>Sets: 5</b>{' '}
 				&nbsp;&nbsp;&nbsp; <b>Reps per set: 5</b>
 				{/* Bad styling practices go <br> (TODO: don't do this) <3 Paul */}
 				<br />
-				<Button style={{ border: '1px solid black' }}>
+				<Button className={classes.EditButton}>
 					<StyledLink to={'/editdayrecord'}>
 						Edit Day Record
 					</StyledLink>
 				</Button>
-			</StyledDiv>
+			</div>
 		);
 	}
 }
