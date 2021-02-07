e219aaa691937b46df626a3b8b075565d29b3cdf
michael-small <small203@morris.umn.edu>
2021-02-07 16:02:23 -0600

    Remove import and center h1 of `AllDayRecords` component

--- src/components/Records/AllDayRecords/AllDayRecords.js
+++ src/components/Records/AllDayRecords/AllDayRecords.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import DayRecord from '../DayRecord/DayRecord';
 import RecordDatePicker from '../RecordDatePicker/RecordDatePicker';
 import CreateRecordButton from '../CreateRecord/CreateRecordButton/CreateRecordButton';
-import { Link } from 'react-router-dom';
 
 export default class AllDayRecords extends Component {
 	render() {
 		return (
 			<div>
-				<h1>AllDayRecords</h1>
+				<h1 style={{ textAlign: 'center' }}>AllDayRecords</h1>
 				{/*To be iterated over*/}
 				<CreateRecordButton></CreateRecordButton>
 
